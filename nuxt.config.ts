4711a71d6b56c64f0f2e4b60b6224befdf8c4dcc
劉彥廷 <eddieliu@liuyantingdeMacBook-Pro.local>
2023-02-01 14:31:22 +0800

    all prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
-import eslint from "vite-plugin-eslint";
+import eslint from 'vite-plugin-eslint'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ["~/assets/css/main.css"],
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   typescript: {
     typeCheck: true,
   },
   vite: {
     plugins: [eslint()],
   },
-});
+})
