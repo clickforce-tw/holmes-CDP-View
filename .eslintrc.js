1fc8ebd1ccd9664e52b0e76d77caf3f3562389af
劉彥廷 <eddieliu@liuyantingdeMacBook-Pro.local>
2023-02-01 15:08:27 +0800

    eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,29 @@ module.exports = {
   extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
   rules: {
     'vue/multi-word-component-names': 'off',
+    'no-tabs': 'off',
+    'import/prefer-default-export': 0,
+    'consistent-return': 'off',
+    'no-alert': 'off',
+    'no-console': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'global-require': 0,
+    'max-len': ['error', { code: 500 }],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'e', // for e.returnvalue
+          'ctx', // for Koa routing
+          'req', // for Express requests
+          'request', // for Express requests
+          'res', // for Express responses
+          'response', // for Express responses
+          'state', // for vuex state
+        ],
+      },
+    ],
   },
 }
