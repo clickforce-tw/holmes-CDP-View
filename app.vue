ce686149d913c1cedd188abada239e5d7ee911aa
劉彥廷 <eddieliu@liuyantingdeMacBook-Pro.local>
2023-02-03 15:27:21 +0800

    layout && page

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
 </template>
 <script>
 import { useCounterStore } from '@/stores/example'
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>CDP環境設定</h1>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
